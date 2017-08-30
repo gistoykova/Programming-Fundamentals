4f0a4af0ef5dd98b77b08b678041ac33f5fc604f
gistoykova <31469311+gistoykova@users.noreply.github.com>
2017-08-30 12:49:56 +0300

    01. Intro and Basic Syntax - Exercises
    Intro and Basic Syntax - Exercises

---
+++ BeverageLabels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beverage_Labels
+{
+    class BeverageLabels
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContentPer100 = int.Parse(Console.ReadLine());
+            int sugarContentPer100 = int.Parse(Console.ReadLine());
+
+            double energyContent = volume * energyContentPer100 / 100.0;
+            double sugarContent = volume * sugarContentPer100 / 100.0;
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", energyContent, sugarContent);
+
+        }
+    }
+}
