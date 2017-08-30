dbff99315e1278636bce009908cde2c482f14a25
gistoykova <g.i.stoykova@gmail.com>
2017-08-30 15:08:05 +0300

    Add files via upload

---
+++ 01. Intro and Basic Syntax - Lab/EmployeeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Data
+{
+    class EmployeeData
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            double monthlySalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Employee ID: {0:D8}", employeeID);
+            Console.WriteLine("Salary: {0:F2}", monthlySalary);
+
+        }
+    }
+}
