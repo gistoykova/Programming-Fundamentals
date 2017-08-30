f07301bd4f959055680f21bb9e77eeecf352d483
gistoykova <g.i.stoykova@gmail.com>
2017-08-30 15:40:04 +0300

    Add files via upload

---
+++ CakeIngredients.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake_Ingredients
+{
+    class CakeIngredients
+    {
+        static void Main(string[] args)
+        {
+            string ingredient = "";
+            int count = 0;
+            while (ingredient != "Bake!")
+            {
+                ingredient = Console.ReadLine();
+                if (ingredient == "Bake!")
+                {
+                    break;
+                }
+                Console.WriteLine("Adding ingredient {0}.", ingredient);
+                count++;
+            }
+            Console.WriteLine("Preparing cake with {0} ingredients.", count);
+
+
+        }
+    }
+}
