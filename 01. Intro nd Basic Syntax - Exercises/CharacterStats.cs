d687f48fe6d8aa3a63c7c855dd109731fa86f35d
gistoykova <g.i.stoykova@gmail.com>
2017-08-30 15:29:35 +0300

    Add files via upload

---
+++ 01. Intro nd Basic Syntax - Exercises/CharacterStats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Stats
+{
+    class CharacterStats
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
+
+            string curHealth = new string('|', currentHealth);
+            string maxHealth = new string('.', maximumHealth - currentHealth);
+            string curEnergy = new string('|', currentEnergy);
+            string maxEnergy = new string('.', maximumEnergy - currentEnergy);
+
+
+            if(currentHealth <= maximumHealth && currentEnergy <= maximumEnergy)
+            {
+                Console.WriteLine("Name: {0}", name);
+                Console.WriteLine("Health: |{0}{1}|", curHealth, maxHealth);
+                Console.WriteLine("Energy: |{0}{1}|", curEnergy, maxEnergy);
+            }
+
+        }
+    }
+}
---
+++ 01. Intro nd Basic Syntax - Exercises/RectangleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Area
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = width * height;
+            Console.WriteLine("{0:F2}", area);
+        }
+    }
+}
